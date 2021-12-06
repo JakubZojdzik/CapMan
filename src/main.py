6a958aea2d63dc7ad4ec473ff189eb44a098f889
JakubZojdzik <jakub.zojdzik@gmail.com>
2021-12-06 13:18:00 +0100

    Basics of player movement

--- src/main.py
+++ src/main.py
@@ -1,16 +1,20 @@
 import pygame
 from colors import Colors
+from win import Win
+from player import Player
 
-WIDTH = 1200
-HEIGHT = 800
-FPS = 30
-BGCOLOR = Colors.CYAN
+Win.BGCOLOR = Colors.CYAN
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+screen = pygame.display.set_mode((Win.WIDTH, Win.HEIGHT))
 pygame.display.set_caption("CapMan Game")
 clock = pygame.time.Clock()
-i = 0
+
+player = Player()
+player_list = pygame.sprite.Group()
+player_list.add(player)
+steps = 10
+
 main = True
 while main:
     for event in pygame.event.get():
@@ -20,13 +24,21 @@ while main:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT or event.key == ord('a'):
+                player.turn(4)
                 print('turn left')
             if event.key == pygame.K_RIGHT or event.key == ord('d'):
+                player.turn(2)
                 print('turn right')
             if event.key == pygame.K_UP or event.key == ord('w'):
+                player.turn(1)
                 print('turn up')
             if event.key == pygame.K_DOWN or event.key == ord('s'):
+                player.turn(3)
                 print('turn down')
- 
+    player.update()
+    screen.fill(Win.BGCOLOR)
+    player_list.draw(screen)
+    pygame.display.flip()
+    clock.tick(Win.FPS)
 
 pygame.quit()
---
+++ src/win.py
@@ -0,0 +1,6 @@
+class Win:
+    WIDTH = 1200
+    HEIGHT = 800
+    FPS = 30
+    BGCOLOR = (0, 0, 0)
+    FPS = 30
