0e00ca1b1e8a6a6c1f8df58ef43dc71d662421ca
JakubZojdzik <jakub.zojdzik@gmail.com>
2021-12-07 18:20:13 +0100

    Full screen size, center the grid with player

--- src/win.py
+++ src/win.py
@@ -1,7 +1,9 @@
 class Win:
-    WIDTH = 1200
-    HEIGHT = 840
+    WIDTH = 1140
+    HEIGHT = 720
     FPS = 30
     BGCOLOR = (0, 0, 0)
     FPS = 30
     GRID_SIZE = 60
+    MARGIN_TOP = 120
+    MARGIN_LEFT = 240
