fceb1769277756cffb98a646e19dea28b47d7355
michalmw2 <michalw.mw2@gmail.com>
2021-12-15 23:24:32 +0100

    Smart scary mode

--- src/ghost.py
+++ src/ghost.py
@@ -38,8 +38,10 @@ img_red = [
     pygame.transform.scale(pygame.image.load("../lib/redghost/redghostleft.png"), [SPRITE_WIDTH, SPRITE_HEIGHT]),
 ]
 
+ghost_poses = [None, None, None, None]
+
 class Ghost(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y, mode, color):
+    def __init__(self, pos_x, pos_y, mode, color, auxiliary_variable=1):
         pygame.sprite.Sprite.__init__(self)
         self.start_pos_x = pos_x
         self.start_pos_y = pos_y
@@ -63,11 +65,29 @@ class Ghost(pygame.sprite.Sprite):
         self.rect.center = (pos_x, pos_y)
         self.direction = 0
         self.prev_mode = "chase"
-        self.auxiliary_variable = 1 # closed -> start_direction , scared -> time_left
+        self.auxiliary_variable = auxiliary_variable # closed -> start_direction , scared -> time_left
+        self.upgrade_ghost_poses()
+
+    def ghost_number(self):
+        if self.color == "red":
+            return 0
+        elif self.color == "pink":
+            return 1
+        elif self.color == "blue":
+            return 2
+        else:
+            return 3
+
+    def upgrade_ghost_poses(self):
+        if self.mode == "closed":
+            ghost_poses[self.ghost_number()] = None
+        else:
+            ghost_poses[self.ghost_number()] = self.rect.center
 
     def resetPos(self):
         self.rect.center = (self.start_pos_x, self.start_pos_y)
         self.direction = 0
+        self.upgrade_ghost_poses()
 
     def is_mode_changed(self):
         if self.mode != self.prev_mode and len(Win.pixelPos_to_gridPos(self.rect.center)) == 1:
@@ -82,8 +102,11 @@ class Ghost(pygame.sprite.Sprite):
                 self.step = 1
 
     @staticmethod
-    def BFS(pos, map, step):
-        cells = Win.pixelPos_to_gridPos(pos)
+    def BFS(pos, map, step, convert_to_gridPos=True, return_length=True):
+        if convert_to_gridPos:
+            cells = Win.pixelPos_to_gridPos(pos)
+        else:
+            cells = pos
         #level_grid = map.show_board().copy()
         visited = copy.deepcopy(map.show_board())
         result = copy.deepcopy(map.show_board())
@@ -103,7 +126,7 @@ class Ghost(pygame.sprite.Sprite):
             if curr_x == -1:
                 curr_x = LVL_WIDTH - 1
 
-            if visited[curr_y][curr_x] == 0 or visited[curr_y][curr_x] == 2:
+            if visited[curr_y][curr_x] in [0, 2, 3]:
                 queue.append([curr_x, curr_y])
                 visited[curr_y][curr_x] = visited[prev_cell[1]][prev_cell[0]]
                 result[curr_y][curr_x] = result[prev_cell[1]][prev_cell[0]] + Win.GRID_SIZE * Win.GRID_SIZE // step
@@ -113,7 +136,7 @@ class Ghost(pygame.sprite.Sprite):
             if curr_y == -1:
                 curr_y = LVL_HEIGHT - 1
 
-            if visited[curr_y][curr_x] == 0 or visited[curr_y][curr_x] == 2:
+            if visited[curr_y][curr_x] in [0, 2, 3]:
                 queue.append([curr_x, curr_y])
                 visited[curr_y][curr_x] = visited[prev_cell[1]][prev_cell[0]]
                 result[curr_y][curr_x] = result[prev_cell[1]][prev_cell[0]] + Win.GRID_SIZE * Win.GRID_SIZE // step
@@ -123,7 +146,7 @@ class Ghost(pygame.sprite.Sprite):
             if curr_x == LVL_WIDTH:
                 curr_x = 0
 
-            if visited[curr_y][curr_x] == 0 or visited[curr_y][curr_x] == 2:
+            if visited[curr_y][curr_x] in [0, 2, 3]:
                 queue.append([curr_x, curr_y])
                 visited[curr_y][curr_x] = visited[prev_cell[1]][prev_cell[0]]
                 result[curr_y][curr_x] = result[prev_cell[1]][prev_cell[0]] + Win.GRID_SIZE * Win.GRID_SIZE // step
@@ -133,12 +156,15 @@ class Ghost(pygame.sprite.Sprite):
             if curr_y == LVL_HEIGHT:
                 curr_y = 0
 
-            if visited[curr_y][curr_x] == 0 or visited[curr_y][curr_x] == 2:
+            if visited[curr_y][curr_x] in [0, 2, 3]:
                 queue.append([curr_x, curr_y])
                 visited[curr_y][curr_x] = visited[prev_cell[1]][prev_cell[0]]
                 result[curr_y][curr_x] = result[prev_cell[1]][prev_cell[0]] + Win.GRID_SIZE / step
 
-        return result
+        if return_length:
+            return result
+        else:
+            return visited
 
     def got_capman(self, capman):
         return self.rect.colliderect(capman.rect)
@@ -185,12 +211,43 @@ class Ghost(pygame.sprite.Sprite):
                     curr_direction = i[3]
                     min_length = BFSed_map[i[1]][i[0]]
             return curr_direction
+            '''
+            ghost_grid_poses = []
+            for i in range(len(ghost_poses)):
+                if (not ghost_poses[i] is None) and (not i == self.ghost_number()):
+                    gridPos = Win.pixelPos_to_gridPos(ghost_poses[i])
+                    for j in gridPos:
+                        ghost_grid_poses.append(j)
+
+            ghost_grid_poses.append(None)
+            #if len(Win.pixelPos_to_gridPos(self.rect.center)) == 1:
+            if True:
+                max_number = -1
+                best_optn = 0
+                for optn in range(len(posibble_moves)):
+                    ghost_grid_poses[-1] = posibble_moves[optn].copy()
+                    BFSed_map2 = Ghost.BFS(ghost_grid_poses, map, 3, False, True)
+                    num_of_cells = 0
+                    for i in range(LVL_HEIGHT):
+                        for j in range(LVL_WIDTH):
+                            if BFSed_map[i][j] > BFSed_map2[i][j]:
+                                num_of_cells += 1
+                    if num_of_cells > max_number:
+                        best_optn = optn
+                        max_number = num_of_cells
+                    print(ghost_poses, ghost_grid_poses, num_of_cells)
+
+                return posibble_moves[best_optn][3]
+            else:
+                return self.direction
+                '''
         elif self.mode == "scared":
             if self.auxiliary_variable <= 0.0:
                 self.mode = "chase"
                 self.is_mode_changed()
                 return self.find_next_move(pos, map)
             self.auxiliary_variable -= 1/Win.FPS
+            '''
             max_length = 0.0
             curr_direction = 1
             for i in posibble_moves:
@@ -198,6 +255,26 @@ class Ghost(pygame.sprite.Sprite):
                     curr_direction = i[3]
                     max_length = BFSed_map[i[1]][i[0]]
             return curr_direction
+            '''
+            if len(Win.pixelPos_to_gridPos(self.rect.center)) == 1:
+                BFSed_map = Ghost.BFS(pos, map, 4, True, True)
+                BFSed_map2 = Ghost.BFS(posibble_moves, map, 2, False, True)
+                BFSed_map3 = Ghost.BFS(posibble_moves, map, 2, False, False)
+                num_of_cells = [0,0,0,0,0]
+                max_number = -1
+                curr_direction = 1
+                for i in range(LVL_HEIGHT):
+                    for j in range(LVL_WIDTH):
+                        if BFSed_map[i][j] <= BFSed_map2[i][j]:
+                            BFSed_map3[i][j] = 9
+                        num_of_cells[BFSed_map3[i][j]-9] += 1
+                for i in range(1, 5):
+                    if num_of_cells[i] > max_number:
+                        max_number = num_of_cells[i]
+                        curr_direction = posibble_moves[i-1][3]
+                return curr_direction
+            else:
+                return self.direction
         else:
             if Win.pixelPos_to_gridPos(self.rect.center)[0] == [12, 12, 0]:
                 self.mode = "chase"
@@ -234,4 +311,6 @@ class Ghost(pygame.sprite.Sprite):
             self.rect.x += Win.WIDTH
         if (self.rect.x > Win.MARGIN_LEFT + Win.WIDTH):
             self.rect.x -= Win.WIDTH
+
+        self.upgrade_ghost_poses()
 
