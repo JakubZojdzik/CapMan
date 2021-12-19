12dfe0ad7afe8fd6ce59077fa70898a674c2a734
PomyslowyZiom <tym2005@outlook.com>
2021-12-19 21:28:53 +0100

    wersja III, capman title

--- src/settings.py
+++ src/settings.py
@@ -16,11 +16,14 @@ class Settings:
       screen = pygame.display.set_mode((screen_info.current_w, screen_info.current_h), pygame.FULLSCREEN)
       width = screen_info.current_w/3
       height = screen_info.current_w/10
+
       margin_top = screen_info.current_h/3
       margin_left = int((screen_info.current_w - width) / 2)
+      self.title= pygame.transform.scale(self.title, [(margin_top-20)*2.4, margin_top-20])
       blackColor = pygame.Color(0, 0, 0)
       yellowColor = pygame.Color(255, 255, 0)
       screen.fill(Win.BGCOLOR)
+      screen.blit(self.title, (((screen_info.current_w - ((margin_top-20)*2.4)) / 2),0))
       # starting position
       x = margin_left+width*self.position
       pygame.draw.rect(screen, yellowColor, Rect(margin_left, margin_top, width, height))
