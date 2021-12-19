9b0de7c7ed4962e9bc4c0eef4b8645f9cbeefcd3
PomyslowyZiom <tym2005@outlook.com>
2021-12-19 20:58:54 +0100

    wersja II, responsywna (raczej)

--- src/settings.py
+++ src/settings.py
@@ -8,33 +8,29 @@ class Settings:
       self.position=0.1
 
    def sound(self):
-      width = 640
-      height = 100
+
 
       # initilaise pygame
       pygame.init()
       screen_info = pygame.display.Info()
-      middle = int((screen_info.current_w - 10) / 2)
-      margin_left = int((screen_info.current_w - width) / 2)
-
-      margin_top = 200
-
-
-
       screen = pygame.display.set_mode((screen_info.current_w, screen_info.current_h), pygame.FULLSCREEN)
+      width = screen_info.current_w/3
+      height = screen_info.current_w/10
+      margin_top = screen_info.current_h/3
+      margin_left = int((screen_info.current_w - width) / 2)
       blackColor = pygame.Color(0, 0, 0)
       yellowColor = pygame.Color(255, 255, 0)
       screen.fill(Win.BGCOLOR)
       # starting position
       x = margin_left+width*self.position
       pygame.draw.rect(screen, yellowColor, Rect(margin_left, margin_top, width, height))
-      pygame.draw.rect(screen, blackColor, Rect(x, margin_top + 5, 10, 90))
+      pygame.draw.rect(screen, blackColor, Rect(x, margin_top + 5, width/40, height-10))
 
       pygame.display.update()
 
-      s = 0
+
       a = x
-      while s == 0:
+      while True:
          button = pygame.mouse.get_pressed()
          if button[0] != 0:
             pos = pygame.mouse.get_pos()
@@ -43,11 +39,11 @@ class Settings:
             a = x - 5
             if a < margin_left:
                a = margin_left
-            if a > margin_left + width - 10:
-               a = margin_left + width - 10
+            if a > margin_left + width - width/40:
+               a = margin_left + width - width/40
             pygame.draw.rect(screen, yellowColor, Rect(margin_left, margin_top, width, height))
             # pygame.display.update(pygame.Rect(0,0,width,height))
-            pygame.draw.rect(screen, blackColor, Rect(a, margin_top + 5, 10, 90))
+            pygame.draw.rect(screen, blackColor, Rect(a, margin_top + 5, width/40, height-10))
             pygame.display.update()
 
          # check for ESC key pressed, or pygame window closed, to quit
@@ -58,5 +54,5 @@ class Settings:
                sys.exit()
             if event.type == pygame.KEYDOWN:
                if event.key == pygame.K_LEFT:  # wraca do menu głównego
-                  self.position=(a - margin_left) / 630
-                  return (a - margin_left) / 630
+                  self.position=(a - margin_left) / width
+                  return (a - margin_left) / width
